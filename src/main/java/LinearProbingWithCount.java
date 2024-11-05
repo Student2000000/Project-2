e5de08913f7bdcd95b0b68406891d63db2783782
Jay A <Jenna.Arends@Seattlecolleges.edu>
2024-11-04 17:28:58 -0800

    Attempting to add LinearProbingWithCount

---
+++ src/main/java/LinearProbingWithCount.java
@@ -0,0 +1,50 @@
+import java.util.Iterator;
+
+//public class BinaryTree<T> implements BinaryTreeInterface<T>
+public class LinearProbingWithCount<T> implements DictionaryInterface <T, T>{
+
+    @Override
+    public Object add(Object key, Object value) {
+        return null;
+    }
+
+    @Override
+    public Object remove(Object key) {
+        return null;
+    }
+
+    @Override
+    public Object getValue(Object key) {
+        return null;
+    }
+
+    @Override
+    public boolean contains(Object key) {
+        return false;
+    }
+
+    @Override
+    public Iterator getKeyIterator() {
+        return null;
+    }
+
+    @Override
+    public Iterator getValueIterator() {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int getSize() {
+        return 0;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+}

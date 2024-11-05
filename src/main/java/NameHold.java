60c357c76a90ba2ac7a938b4d36327c984775fc2
Jay A <Jenna.Arends@Seattlecolleges.edu>
2024-11-04 17:31:51 -0800

    Adding my additional class NameHold

---
+++ src/main/java/NameHold.java
@@ -0,0 +1,38 @@
+import java.util.Set;
+
+/**
+ * Using encapuslation the class takes the data from NameGen and holds it
+ * for use
+ */
+
+public class NameHold {
+    private Set<String> names1000;
+    private Set<String> names10000;
+
+    /**
+     * Constuctor
+     * @param amt1   amount of names to be genorated
+     * @param amt2   amount of names to be genorated
+     * @param lengthOfName   length of the specific name
+     */
+    public NameHold(int amt1, int amt2, int lengthOfName) {
+        this.names1000 = NameGen.nameSetCreate(amt1, lengthOfName);
+        this.names10000 = NameGen.nameSetCreate(amt2, lengthOfName);
+    }
+
+    /**
+     * Contrcutor
+     * @return   the 1000 name populated list
+     */
+    public Set<String> getNames1000() {
+        return names1000;
+    }
+
+    /**
+     * Contuctor
+     * @return   the 10,000 name populated list
+     */
+    public Set<String> getNames10000() {
+        return names10000;
+    }
+}

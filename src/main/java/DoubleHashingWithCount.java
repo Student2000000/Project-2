3a57935bed763c3e6537be49fa9debc34e04491d
Jay A <Jenna.Arends@Seattlecolleges.edu>
2024-11-04 17:27:20 -0800

    Added DoubleHashingWithCount and hopefully the new file stucture

---
+++ src/main/java/DoubleHashingWithCount.java
@@ -0,0 +1,50 @@
+import java.util.Iterator;
+
+//public class BinaryTree<T> implements BinaryTreeInterface<T>
+public class DoubleHashingWithCount<T> implements DictionaryInterface<T, T> {
+
+    @Override
+    public T add(T key, T value) {
+        return null;
+    }
+
+    @Override
+    public T remove(T key) {
+        return null;
+    }
+
+    @Override
+    public T getValue(T key) {
+        return null;
+    }
+
+    @Override
+    public boolean contains(T key) {
+        return false;
+    }
+
+    @Override
+    public Iterator<T> getKeyIterator() {
+        return null;
+    }
+
+    @Override
+    public Iterator<T> getValueIterator() {
+        return null;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int getSize() {
+        return 0;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+}
